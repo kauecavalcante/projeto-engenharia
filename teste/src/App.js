6db10e74adc0a33f6e193448065a5a8616d150ae
Kauê Cavalcante <Kauecavalcante4@gmail.com>
2024-08-29 20:06:16 -0300

    testando a branch e o react
    Co-Authored-By: JoaoPedro-09 <178346626+JoaoPedro-09@users.noreply.github.com>
    Co-Authored-By: Pedro Teixeira <129986058+pedroteixeira02@users.noreply.github.com>
    Co-Authored-By: marjufeitosa <162568486+marjufeitosa@users.noreply.github.com>

---
+++ teste/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = "kaue"
+  return (
+    <div className="App">
+      <h1>TEste teixeira gay</h1>
+      <p>Russinho feio</p>
+      <p>Meu nome é {name}</p>
+      <h2>teste</h2>
+     
+    </div>
+  );
+}
+
+export default App;
